de5758f5afb9231a8f88bd58b35f817e8e60578f
Sandeep Panigrahi <sandeep0909@yahoo.com>
2016-08-15 21:00:01 -0700

    Initial Commit

---
+++ app.js
@@ -0,0 +1,9 @@
+
+<SCRIPT LANGUAGE="JavaScript">
+test = new Date()
+month = test.getMonth()
+month = (month * 1) + 1
+day = test.getDate()
+year = test.getFullYear()
+document.write(" ",month,"/",day,"/",year," ")
+</SCRIPT>
